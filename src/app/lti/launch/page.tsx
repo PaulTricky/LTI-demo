fcaf5d5808df3d34225127bf23d3290c5ad9d13c
Paul <>
2024-06-23 09:31:17 +0700

    feat: add lti launch

--- src/app/lti/launch/page.tsx
+++ src/app/lti/launch/page.tsx
@@ -1,8 +1,10 @@
 const Launch = async () => {
 
   const getToken = async () => {
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/idtoken`);
-    return res.json();
+    try {
+      const res = await fetch(`${process.env.NEXTAUTH_URL}/api/idtoken`);
+      return res.json();
+    } catch(e) {}
   }
 
   const token = await getToken();
