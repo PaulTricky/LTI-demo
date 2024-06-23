ec8631339d903a741fa5f8fda136c7400140c77c
Paul <>
2024-06-23 10:29:27 +0700

    fix: add x_ltik header

--- src/app/api/idtoken/route.ts
+++ src/app/api/idtoken/route.ts
@@ -1,8 +1,9 @@
 import { getLtikAuthHeader } from '@/app/configs/serverConfig';
+import { NextRequest } from 'next/server';
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const x_ltik = searchParams.get('x_ltik') as string;
+export async function GET(request: NextRequest) {
+  const x_ltik = request.headers.get('x_ltik') as string;
+  console.log('x_ltik', x_ltik, getLtikAuthHeader(x_ltik));
   const url = `${process.env.LTIAAS_URL}/api/idtoken`;
   const res = await fetch(url, {
     headers: {
--- src/app/lti/launch/page.tsx
+++ src/app/lti/launch/page.tsx
@@ -1,11 +1,19 @@
 import axios from "axios";
+import { headers } from "next/headers";
+import { useSearchParams } from "next/navigation";
 
 const Launch = async () => {
 
   const getToken = async () => {
+    const headersQuery = headers()
+    const url = new URL(headersQuery.get("referer") as string);
+    const ltik = url.searchParams.get("ltik");
     try {
       const res = await axios(`${process.env.NEXTAUTH_URL}/api/idtoken`, {
         method: 'GET',
+        headers: {
+          'x_ltik': ltik
+        }
       });
       return res.data.data
     } catch(e) {
