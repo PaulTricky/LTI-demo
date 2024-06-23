a4237b1e16580b053d8122ff8e1bf572f53d4837
Paul <>
2024-06-23 09:38:52 +0700

    fix: add try catch

--- src/app/lti/launch/page.tsx
+++ src/app/lti/launch/page.tsx
@@ -4,12 +4,16 @@ const Launch = async () => {
     try {
       const res = await fetch(`${process.env.NEXTAUTH_URL}/api/idtoken`);
       return res.json();
-    } catch(e) {}
+    } catch(e) {
+      console.log("eeeee", e)
+    }
   }
 
   const token = await getToken();
 
   console.log("token", token);
+
+  if (!token) return null;
 
   return (
     <div>{JSON.stringify(token)}</div>
